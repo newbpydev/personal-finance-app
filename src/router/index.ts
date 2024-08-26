a131340b8b0056a10383c346665092044e20b6e4
Juan Gomez <94386361+newbpydev@users.noreply.github.com>
2024-08-26 14:04:35 -0300

    initial commit

---
+++ src/router/index.ts
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import OverviewView from '@/views/OverviewView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'overview',
+      component: OverviewView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/OverviewView.vue')
+    }
+  ]
+})
+
+export default router
