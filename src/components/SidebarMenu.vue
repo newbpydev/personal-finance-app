681da33ac2b1fd81178ac8a49de47f9784ad515f
Juan Gomez <94386361+newbpydev@users.noreply.github.com>
2024-08-28 22:22:09 -0300

    feat(fonts): add Open Font License (OFL) text file for Public Sans font feat(fonts): add variable font files for Public Sans font feat(fonts): add README file for Public Sans font feat(fonts): add static font files for Public Sans font feat(images): add avatar images for various profiles
    feat(assets): add new avatar images for various users
    feat(assets): add new icons for bill due, bill paid, and caret directions
    
    feat(images): add new SVG icons for close modal, ellipsis, filter mobile, and hide password to enhance UI experience
    
    feat(images): add new SVG icons for minimizing menu, budgets, and overview in the application navigation bar
    
    feat(icons): add new SVG icons for navigation menu items: pots, recurring bills, and transactions
    
    feat(icons): add new SVG icons for pot and recurring bills to the project
    
    feat(images): add new SVG icons for search, selected, show password, and sort mobile to enhance UI experience
    
    feat(svg): add SVG markup for an image with masks and clip paths to enhance visual appearance
    
    refactor: optimize SVG paths in the file to reduce redundancy and improve readability
    
    chore: update SVG paths in the file to improve readability and maintain consistency in the codebase
    
    chore: update SVG paths in the file to improve readability and maintain consistency in the codebase
    
    chore: optimize SVG paths in the file to reduce redundancy and improve readability
    
    style: format SVG paths in the file to improve readability and maintain consistency
    
    refactor: clean up SVG paths in the file to improve readability and maintainability
    
    chore: update SVG paths in the file to improve readability and maintain consistency in the codebase
    
    refactor: optimize SVG paths in the file for better readability and maintainability
    
    chore: update SVG paths in the file to improve readability and maintainability
    
    refactor: optimize SVG path data in the file for better readability and maintainability
    
    refactor: optimize SVG paths in the file to improve readability and maintainability
    
    refactor: optimize SVG paths in the file to improve readability and maintainability
    
    feat(images): add new SVG illustration for authentication page to enhance visual appeal and user experience
    
    feat(assets): add new large and small logo SVG files for branding purposes
    style(styles): update color palette and spacing variables for a more consistent design system
    
    style(main.scss): reorganize imports in main.scss for better readability and maintainability
    feat(main.scss): add new styles and responsive layout for #app element in main.scss
    feat(reset.scss): add comprehensive CSS reset styles for improved cross-browser consistency
    feat(utils.scss): add mixins for text presets and responsive styles in utils.scss
    feat(variables.scss): add variables for mobile, laptop, and desktop widths in variables.scss
    feat(SidebarMenu.vue): refactor script setup to script lang="ts" setup in SidebarMenu.vue
    feat(SidebarMenu.vue): add computed properties and event listeners for screen width in SidebarMenu.vue
    feat(SidebarMenu.vue): update template structure and add header content in SidebarMenu.vue
    feat(SidebarMenu.vue): add styles for header and navigation in SidebarMenu.vue
    feat(BudgetsIcon.vue): create BudgetsIcon component with script, template, and scoped styles
    
    feat(icons): add OverviewIcon, PotsIcon, RecurringBillsIcon, and TransactionsIcon components to the project for displaying different icons in the UI

--- src/components/SidebarMenu.vue
+++ src/components/SidebarMenu.vue
@@ -1,11 +1,139 @@
-<script setup lang="ts">
+<script lang="ts" setup>
+import { computed, onMounted, onUnmounted, ref } from 'vue'
+import OverviewIcon from '@/components/icons/OverviewIcon.vue'
+
+const isMenuMinimized = ref(false)
+const isTest = computed(() => {
+  return window.innerWidth
+})
+
+const isDesktop = ref(window.innerWidth >= 1440)
+
+onMounted(() => {
+  window.addEventListener('resize', updateScreenWidth)
+})
+onUnmounted(() => {
+  window.removeEventListener('resize', updateScreenWidth)
+})
+
+const updateScreenWidth = () => {
+  isDesktop.value = window.innerWidth >= 1440
+}
 
 </script>
 
 <template>
-  $END$
+  <header>
+    <picture v-show="isDesktop">
+      <source media="(min-width: )" srcset="">
+      <img alt="finance company logo" src="../assets/images/logo-large.svg" />
+    </picture>
+
+    <nav>
+      <ul class="navigation-list">
+        <li class="navigation-item">
+          <router-link :to="{name: 'overview'}" class="navigation-link">
+            <OverviewIcon />
+            <span>Overview</span>
+          </router-link>
+        </li>
+        <li class="navigation-item">
+          <router-link :to="{name: 'transactions'}" class="navigation-link">
+            <img alt="icon nav transactions" src="../assets/images/icon-nav-transactions.svg" title="Transactions">
+            <span>Transactions</span>
+          </router-link>
+        </li>
+        <li class="navigation-item">
+          <router-link :to="{name: 'budgets'}" class="navigation-link">
+            <img alt="icon nav budgets" src="../assets/images/icon-nav-budgets.svg" title="Budgets">
+            <span>Budgets</span>
+          </router-link>
+        </li>
+        <li class="navigation-item">
+          <router-link :to="{name: 'pots'}" class="navigation-link">
+            <img alt="icon nav pots" src="../assets/images/icon-nav-pots.svg" title="Pots">
+            <span>Pots</span>
+          </router-link>
+        </li>
+        <li class="navigation-item">
+          <router-link :to="{name: 'recurring-bills'}" class="navigation-link">
+            <img alt="icon nav recurring bills" src="../assets/images/icon-nav-recurring-bills.svg"
+                 title="Recurring Bills">
+            <span>Recurring Bills</span>
+          </router-link>
+        </li>
+      </ul>
+    </nav>
+
+    <button v-show="isDesktop">Minimize Menu</button>
+  </header>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+@import '../assets/styles/utils.scss';
+
+header {
+  background-color: var(--color-background-dark);
+  display: flex;
+  gap: var(--spacing-6, 2.4rem);
+  color: var(--c-grey-300);
+  @include responsive(border-top-left-radius, var(--border-radius-sm), none, 0);
+  @include responsive(border-top-right-radius, var(--border-radius-sm), none, 0);
+
+  & nav {
+    width: 100%;
+  }
+
+  .navigation {
+    &-list {
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+      @include responsive(flex-direction, row, none, column);
+      padding: var(--spacing-2, .8rem) var(--spacing-4, 1.6rem) 0 var(--spacing-4, 1.6rem);
+    }
+
+    &-item {
+      display: block;
+      width: 100%;
+      height: 100%;
+    }
+
+    &-link {
+      display: flex;
+      @include text-preset-5-bold();
+      //@include responsive(flex-direction, row, row);
+      //flex-direction: column;
+      @include responsive(flex-direction, column, none, row);
+      justify-content: center;
+      align-items: center;
+      padding: var(--spacing-2, .8rem) 0 var(--spacing-3, 1.2rem);
+
+      &.router-link-active {
+        background-color: var(--c-beige-100);
+        color: var(--c-grey-900);
+        @include responsive(border-top-left-radius, var(--border-radius-sm), none, 0);
+        @include responsive(border-top-right-radius, var(--border-radius-sm), none, 0);
+        @include responsive(border-bottom, var(--spacing-1, .4rem) solid var(--c-theme-green));
+
+        fill: var(--c-theme-green);
+      }
+
+      & img {
+        display: block;
+        height: 2.4rem;
+      }
+
+      & span {
+        display: none;
+        @media (min-width: 45em) {
+          display: block;
+        }
+      }
+
+    }
+
+  }
+}
 
 </style>
