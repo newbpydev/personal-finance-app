7702f656ddce29e48a23e858dbc98e3941a0d26e
Juan Gomez <94386361+newbpydev@users.noreply.github.com>
2024-09-10 21:27:01 -0300

    feat(SearchField.vue): add a new search input component for user input feat(RecurringBillsList.vue): create a new component for displaying recurring bills chore(RecurringBills.vue): remove unused RecurringBills component to clean up codebase

---
+++ src/components/inputs/SearchField.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+const model = defineModel('value', { type: String })
+defineProps<{ placeholder }>()
+
+</script>
+
+<template>
+    <input v-model="model" :placeholder="placeholder" class="searchbar" type="text">
+</template>
+
+<style lang="scss" scoped>
+//.searchbar
+
+</style>
---
+++ src/components/recurring-bills/RecurringBillsList.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <section class="bills">
+        <div class="search-options">
+        
+        </div>
+
+        <div class="bills-table">
+            <ul class="bills-list">
+                <li class="list-item">
+
+                </li>
+            </ul>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
