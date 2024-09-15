3799a2f327bae271090dc36d85a35a6b0a5889a7
Juan Gomez <94386361+newbpydev@users.noreply.github.com>
2024-09-14 21:07:49 -0300

    feat(dates.ts): enhance getDay function to return day with ordinal suffixes for better readability

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -6,6 +6,23 @@ export const formatDate = (dateString: string, locale: string = 'en-UK') => {
 
 export const getDay = (dateString: string, locale: string = 'en-UK') => {
   const date = new Date(dateString)
-  const options = { day: '2-digit' }
-  return new Intl.DateTimeFormat(locale, options).format(date)
+  const options = { day: 'numeric' }
+  const res = Number(new Intl.DateTimeFormat(locale, options).format(date))
+
+  switch (res) {
+    case 1:
+    case 21:
+    case 31 :
+      return `${res}st`
+    case 2:
+    case 22:
+      return `${res}nd`
+    case 3:
+    case 23:
+      return `${res}rd`
+    default:
+      return `${res}th`
+  }
+
+  // return new Intl.DateTimeFormat(locale, options).format(date)
 }
