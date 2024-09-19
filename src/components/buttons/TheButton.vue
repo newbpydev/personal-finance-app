6451363ce66897b09597d74b7829b4d5cfe053ee
Juan Gomez <94386361+newbpydev@users.noreply.github.com>
2024-09-19 20:36:28 -0300

    feat(TheButton.vue): add a new button component with variant props for better UI flexibility

---
+++ src/components/buttons/TheButton.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+defineProps<{
+    variant: 'primary' | 'secondary' | 'destroy'
+}>()
+</script>
+
+<template>
+    <button class="btn">
+        <slot />
+    </button>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
